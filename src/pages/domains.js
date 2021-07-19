6525d713be24b31bcff6a0f28db1c083e9e326b7
MAHE <divyamanipal01@gmail.com>
2021-07-19 11:51:09 +0530

    Domain page changes

--- src/pages/domains.js
+++ src/pages/domains.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from "react";
 import Slider from "react-slick";
 import { Container} from 'react-bootstrap';
@@ -13,6 +13,7 @@ import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
 
 const domains = [
+
 {   "image":cp,
     "title":"Coding",
     "desc":`Enter into the coding arena  to test your daring.
@@ -55,13 +56,70 @@ const domains = [
     prompt and emerge victorious.`,
      "time":"23"
 },
+
+{   "image":cp,
+    "title":"Coding",
+    "desc":`Enter into the coding arena  to test your daring.
+    Courage in the face of the unknown is an important quality of any coder. 
+    Race against time to solve the problems and secure your position at the top of the 
+    leaderboard.`,
+    "time":"12"
+},
+{
+    "image":ml,
+    "title":"djd",
+    "desc":`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
+    do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit`,
+     "time":"12"
+},
+{
+    "image":elec,
+    "title":"Electronics",
+    "desc":`History has seen titans and emperors battle their hearts out on the arena, 
+    filling the arena with the energy of their battle cry and clash of weapons.
+    It's time to rewrite history our way. So go out in that rising sun with your circuitry 
+    skills sharp and ready to clash, devices crackling in arrival and fill the arena with
+     electrifying energy. Battleface On!`,
+     "time":"12"
+},
+{
+    "image":webdev,
+    "title":"Web Development",
+    "desc":`Welcome to the Time Travellers Developers Section.
+    New is good but the Old is gold for a reason. Bring out the developer in you with an 
+    exhilarating race back to the good old days. Prove your mettle and you shall be rewarded.`,
+     "time":"11"
+},
+{   "image":appdev,
+    "title":"App Development",
+    "desc":`Enter the app development arena to develop a full-stack or a front-end app. 
+    Race against time with your arsenal of tech weaponry to develop an app with the given 
+    prompt and emerge victorious.`,
+     "time":"23"
+},
+
 ]
 
 export default function Domain(){
+const [wwidth,setWwidth] = useState(window.innerWidth);
+const [items,setItems] = useState(3);
+
+useEffect(()=>{
+  console.log(wwidth);
+  if(wwidth>768){
+    setItems(5);
+  }
+  else{
+    setItems(3);
+  }
+},[wwidth]);
+
     const NextArrow = ({ onClick }) => {
         return (
           <div className="arrow next" onClick={onClick}>
-            <FaArrowRight/>
+            <FaArrowRight className="arrow-icon"/>
           </div>
         );
       };
@@ -69,7 +127,7 @@ export default function Domain(){
       const PrevArrow = ({ onClick }) => {
         return (
           <div className="arrow prev" onClick={onClick}>
-            <FaArrowLeft/>
+            <FaArrowLeft className="arrow-icon"/>
           </div>
         );
       };
@@ -77,12 +135,10 @@ export default function Domain(){
 
       const settings = {
         infinite: true,
-        dots:true,
-        dotsClass:'slick-dots',
         lazyLoad: true,
         focusOnSelect:true,
         speed: 300,
-        slidesToShow: 3,
+        slidesToShow: items,
         centerMode: true,
         centerPadding: "0",
         nextArrow: <NextArrow />,
@@ -100,9 +156,9 @@ export default function Domain(){
           <div key={idx} className={idx === imageIndex ? "slide activeSlide" : "slide"}>
             <img src={domain.image} alt={domain} />
             <div className="domain-info">
-            <h6>{domain.title}</h6>
+            <h6 className="h2-color">{domain.title}</h6>
             <p>{domain.desc}</p>
-            <p>{domain.time}</p>
+            <p className="h2-color">{domain.time}</p>
             </div>
           </div>
         ))}
